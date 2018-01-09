0f111e02d2370dab244b961587e0850d291dd24f
DaveParillo <DaveParillo@users.noreply.github.com>
2018-01-08 17:10:18 -0800

    add new script

---
+++ publish.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -rf build/intermediate-cpp/
+mkdir -p build/intermediate-cpp/
+cd build/intermediate-cpp/
+
+git init
+git remote add book https://DaveParillo@github.com/DaveParillo/intermediate-cpp.git
+git pull --depth=1 book gh-pages
+
+cd ../..
+runestone build
+
+cd build/intermediate-cpp/
+# instruct github to process directories starting with underscores
+touch .nojekyll
+git add --all
+git commit -m 'publish update to gh-pages'
+git merge --no-edit -s ours remotes/book/gh-pages
+git push book master:gh-pages
+
