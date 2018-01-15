c13d38fbe4c83044b58fc282aeb713d15dd501d2
DaveParillo <DaveParillo@users.noreply.github.com>
2018-01-14 21:10:58 -0800

    added extension

--- conf.py
+++ conf.py
@@ -27,7 +27,9 @@ from runestone import runestone_static_dirs, runestone_extensions
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.mathjax', 'sphinx.ext.graphviz', 'cppreference'] + runestone_extensions()
+extensions = ['sphinx.ext.mathjax', 
+              'sphinx.ext.graphviz', 
+              'cppreference'] + runestone_extensions()
 
 #,'runestone.video','runestone.reveal','runestone.poll','runestone.tabbedStuff','runestone.disqus','runestone.codelens','runestone.activecode', 'runestone.assess', 'runestone.animation','runestone.meta', 'runestone.parsons', 'runestone.blockly', 'runestone.livecode']
 
